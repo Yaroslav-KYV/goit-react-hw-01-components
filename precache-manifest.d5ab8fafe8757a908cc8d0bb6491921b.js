dcec15c6c85b0d9921db8d2d65573a7badbb7606
Yaroslav <kovshun.yv@gmail.com>
2019-11-20 13:39:06 +0200

    Updates

---
+++ precache-manifest.d5ab8fafe8757a908cc8d0bb6491921b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5849e0c7287e1758e59f28258b7deaa7",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "83adc4b1178f2e601115",
+    "url": "/goit-react-hw-01-components/static/css/main.4cea202b.chunk.css"
+  },
+  {
+    "revision": "de66e3acf506aa908245",
+    "url": "/goit-react-hw-01-components/static/js/2.c1d859a5.chunk.js"
+  },
+  {
+    "revision": "83adc4b1178f2e601115",
+    "url": "/goit-react-hw-01-components/static/js/main.d314d7ac.chunk.js"
+  },
+  {
+    "revision": "76c0918716ad9d7cd3e5",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.17fd7762.js"
+  }
+]);
